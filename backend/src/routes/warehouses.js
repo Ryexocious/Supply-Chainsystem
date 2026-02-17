8cb581af642faf4395e687a72e47bce54a8be012
Ryexocious <saibulhaqjessan98@gmail.com>
2026-02-17 20:34:18 +0600

    routesupdated

--- backend/src/routes/warehouses.js
+++ backend/src/routes/warehouses.js
@@ -1,7 +1,14 @@
 const express = require('express');
 const router = express.Router();
 const warehouseController = require('../controllers/warehouseController');
+const { authMiddleware } = require('../middleware/auth');
+
+router.use(authMiddleware);
 
 router.get('/', warehouseController.getAllWarehouses);
+router.get('/:id', warehouseController.getWarehouseById);
+router.post('/', warehouseController.createWarehouse);
+router.put('/:id', warehouseController.updateWarehouse);
+router.delete('/:id', warehouseController.deleteWarehouse);
 
 module.exports = router;
